2ef8ce6e94200f6da35456322a185e30a872475b
LAPTOP-TS3NINN8\81902 <ikejose0@gmail.com>
2020-12-23 14:59:22 +0900

    Added Hittable Lists

---
+++ HittableList.cpp
@@ -0,0 +1,36 @@
+#include "HittableList.h"
+
+HittableList::HittableList()
+{
+}
+
+HittableList::HittableList(shared_ptr<Hittable> object) {
+	add(object);
+}
+
+void HittableList::clear()
+{
+	objects.clear();
+}
+
+void HittableList::add(shared_ptr<Hittable> object)
+{
+	objects.push_back(object);
+}
+
+bool HittableList::hit(const Ray& r, double t_min, double t_max, hit_record& rec) const
+{
+    hit_record temp_rec;
+    bool hit_anything = false;
+    auto closest_so_far = t_max;
+
+    for (const auto& object : objects) {
+        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
+}
---
+++ HittableList.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "hittable.h"
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class HittableList : public Hittable
+{
+public: 
+	HittableList();
+	HittableList(shared_ptr<Hittable> object);
+	void clear();
+	void add(shared_ptr<Hittable> object);
+	virtual bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const override;
+public:
+	std::vector<shared_ptr<Hittable>> objects;
+
+};
+
