1a1b9a79d4ccbefe67aeb04c173b3097cf3cfcbe
LAPTOP-TS3NINN8\81902 <ikejose0@gmail.com>
2022-06-23 14:58:53 +0900

    Slight reorganization of files.

--- FileResources.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-using namespace std;
-#pragma once
-
-const static string MOON_FILE = "extern\\img\\moon.jpg";
-const static string SKYBOX_FILE = "extern\\img\\daysky.jpg";
---
+++ src/FileResources.h
@@ -0,0 +1,6 @@
+#include <string>
+using namespace std;
+#pragma once
+
+const static string MOON_FILE = "..\\extern\\img\\moon.jpg";
+const static string SKYBOX_FILE = "..\\extern\\img\\daysky.jpg";
